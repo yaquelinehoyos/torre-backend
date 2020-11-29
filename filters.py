4cb02829ae9589bdcbaa49efc1590b1b1374c0b3
Yaqueline Hoyos <yaqueline150@gmail.com>
2020-11-29 15:03:13 -0500

    filters.py created

---
+++ filters.py
@@ -0,0 +1,5 @@
+filters = {
+    "frontend": ["Vue", "React", "Angular"],
+    "backend": ["Laravel", "Django", "FastAPI", "Ruby on rails", "Flask", "NodeJs"],
+    "computer_science": ["History", "Theorical computer science", "Computer systems and computacional processes", "Artificial intelligence", "Applied computer science"]
+}
--- main.py
+++ main.py
@@ -2,30 +2,28 @@ from fastapi import FastAPI
 import requests
 import json
 from starlette.middleware.cors import CORSMiddleware
+import filters
 
 app = FastAPI()
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins="*",
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/bio/{bio_id}")
-async def read_bio(bio_id):
-    torre_bio_url = "https://torre.bio/api/bios/"+bio_id
+
+@app.get("/bio/{username}")
+async def read_bio(username):
+    torre_bio_url = "https://torre.bio/api/bios/"+username
     torre_bio = requests.get(torre_bio_url)
     if torre_bio.status_code == 200:
         data = json.loads(torre_bio.content.decode('utf-8'))
         return {"message": data}
 
+
 @app.get("/content/filters")
 async def read_filters():
-        filters = {
-            "frontend" : [ "Vue", "React", "Angular" ],
-            "backend" : [ "Laravel", "Django", "FastAPI", "Ruby on rails", "Flask", "NodeJs" ],
-            "computer_science" : [ "History", "Theorical computer science", "Computer systems and computacional processes", "Artificial intelligence", "Applied computer science" ]
-         }
-        return {"message": filters}
+    return {"message": filters.filters}
